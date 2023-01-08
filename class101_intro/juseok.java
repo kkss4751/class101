61207c88073471ec45a807fab5526b6c300a3967
Jeonghun-Lee <kkss4751@naver.com>
2023-01-08 23:40:20 +0900

    class101 자바입문부터 실무까지

---
+++ class101_intro/juseok.java
@@ -0,0 +1,24 @@
+package class101_intro;
+
+public class juseok {
+    //hello world 출력 주석 연습
+    //23.01.08
+    public static void main(String[] args) {
+        // Hello world를 출력한다
+        System.out.println("Hello world");
+        // 개행을 하지 않고 Hello를 출력한다.
+        System.out.print("Hello");
+        // 이어서 world를 출력한다
+        System.out.print("world");
+
+
+        /* 여러 줄 주석은 이렇게 처리한다 */
+        /** 여러 줄은 이렇게 처리한다
+         * 색깔이 들어가는건 javadoc 에 들어간다
+         * */
+
+        
+
+
+    }
+}
